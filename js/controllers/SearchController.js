9472ecf45da8adf4007ce38709125fdf0b259688
Prabhakar Doraisamy <prabhutech@yahoo.com>
2013-11-08 01:57:37 -0800

    Location location location
    - track restaurants using yelp and plot using gmaps api

--- js/controllers/SearchController.js
+++ js/controllers/SearchController.js
@@ -1,13 +1,58 @@
 SporkitApp.controller('SearchController', ['$scope', 'Facebook', 'Utils',
 function($scope, Facebook, Utils) {
-
+    var map;
+    var geocoder = new google.maps.Geocoder();
+    var infowindow = new google.maps.InfoWindow();
     $scope.$on('search-restaurants-onsuccess', function(event, response) {
         console.log(response);
         $scope.nearByRestaurants = response;
+        var restaurant_locations = [];
+        for (var i = 0; i < $scope.nearByRestaurants.businesses.length; i++) {
+            var loc = [];
+            loc.push($scope.nearByRestaurants.businesses[i].name);
+            loc.push($scope.nearByRestaurants.businesses[i].location.display_address[0] + ', ' + $scope.nearByRestaurants.businesses[i].location.display_address[1]);
+            restaurant_locations.push(loc);
+        };
+
+        $scope.plotLocationsOnMap(restaurant_locations);
         if (!$scope.$$phase) {
             $scope.$apply();
         }
     });
+    $scope.plotLocationsOnMap = function(restaurant_locations) {
+        for (var i = 0; i < restaurant_locations.length; i++) {
+            codeAddresses(restaurant_locations[i]);
+            
+        }
+    };
+
+    function codeAddresses(arr) {
+        geocoder.geocode({
+            'address' : arr[1]
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                marker = new google.maps.Marker({
+                    map : map,
+                    position : results[0].geometry.location,
+                    title : arr[0],
+                    icon : 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+                });
+                makeInfoWindowEvent(map, infowindow, arr[0], marker);
+                //markersArray.push(marker);
+            } else {
+                console.log("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    }
+
+    function makeInfoWindowEvent(map, infowindow, contentString, marker) {
+        google.maps.event.addListener(marker, 'mouseover', function() {
+            infowindow.setContent(contentString);
+            infowindow.open(map, marker);
+        });
+    }
+
+
     $scope.isLoading = true;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -26,14 +71,14 @@ function($scope, Facebook, Utils) {
 
         var myOptions = {
             center : latlon,
-            zoom : 14,
+            zoom : 16,
             mapTypeId : google.maps.MapTypeId.ROADMAP,
             mapTypeControl : false,
             navigationControlOptions : {
                 style : google.maps.NavigationControlStyle.SMALL
             }
         };
-        var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+        map = new google.maps.Map(document.getElementById("mapholder"), myOptions)
         var marker = new google.maps.Marker({
             position : latlon,
             map : map,
@@ -42,7 +87,7 @@ function($scope, Facebook, Utils) {
         $scope.isLoading = false;
 
         Facebook.callYelp({
-            'action' : 'http://api.yelp.com/v2/search?term=panda&ll=' + lat + ',' + lon,
+            'action' : 'http://api.yelp.com/v2/search?term=restaurant&ll=' + lat + ',' + lon,
             'method' : 'GET',
             'callbackId' : 'search-restaurants'
         });
@@ -69,6 +114,7 @@ function($scope, Facebook, Utils) {
         }
         $scope.isLoading = false;
     }
+
 
     $scope.$on('getLocationFromLL-onsuccess', function(event, response) {
         $scope.currentLocationAddress = response[0].formatted_address;
